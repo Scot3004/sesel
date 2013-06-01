93a9287de63c1b41dda7fb9bcacb0dd2add272ab
Sergio C. Orozco Torres <scot3004@gmail.com>
2013-06-01 14:18:50 -0500

    Revert "Gestión de estudiantes"
    This reverts commit ef695367480ee5cb7186012494e2b89e02a9e944.

--- application/views/noautorizado.php
+++
@@ -1,3 +0,0 @@
-<h1>No estás autorizado para acceder a la página</h1>
-<p>La página <?php echo anchor(current_url())?> que solicitas requiere que inicies sesion</p>
-<a href="<?php echo site_url('usuario/login')?>" data-role="button" data-icon="check" data-theme="a">Iniciar Sesion</a>
